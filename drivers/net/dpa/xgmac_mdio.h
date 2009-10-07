1c335dd69db3352ed3eb40c624d57aeb2f252b60
Andy Fleming <afleming@freescale.com>
2009-10-07 03:32:04 -0500

    Rework the DPA PHY code
    * Add 10G mdio support
    * Move PHY interaction into MAC code
    * Remove mac_device's adjust_link function
    * Add an init_phy function to the mac_device struct
    
    Signed-off-by: Andy Fleming <afleming@freescale.com>

--- drivers/net/dpa/xgmac_mdio.h
+++ drivers/net/dpa/xgmac_mdio.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2008-2009 Freescale Semiconductor, Inc. All rights reserved.
+/* Copyright (C) 2009 Freescale Semiconductor, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -27,57 +27,35 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Freescale FMAN XGMAC MDIO Driver -- MDIO Management Bus Implementation
+ * Driver for the MDIO bus controller on QorIQ 10G ports
+ *
+ * Author: Andy Fleming
  */
 
-#ifndef __MAC_H
-#define __MAC_H
+#ifndef __XGMAC_MDIO_H
+#define __XGMAC_MDIO_H
 
-#include <linux/device.h>	/* struct device, BUS_ID_SIZE */
-#include <linux/if_ether.h>	/* ETH_ALEN */
-#include <linux/phy.h>		/* phy_interface_t, struct phy_device */
+struct tgec_mdio_controller {
+	u32	res0[0xc];
+	u32	mdio_stat;	/* MDIO configuration and status */
+	u32	mdio_ctl;	/* MDIO control */
+	u32	mdio_data;	/* MDIO data */
+	u32	mdio_addr;	/* MDIO address */
+} __attribute__ ((packed));
 
-#include "fsl_fman.h"		/* struct port_device */
+#define MDIO_STAT_CLKDIV(x)	(((x>>1) & 0xff) << 8)
+#define MDIO_STAT_BSY		(1 << 0)
+#define MDIO_STAT_RD_ER		(1 << 1)
+#define MDIO_CTL_DEV_ADDR(x) 	(x & 0x1f)
+#define MDIO_CTL_PORT_ADDR(x)	((x & 0x1f) << 5)
+#define MDIO_CTL_PRE_DIS	(1 << 10)
+#define MDIO_CTL_SCAN_EN	(1 << 11)
+#define MDIO_CTL_POST_INC	(1 << 14)
+#define MDIO_CTL_READ		(1 << 15)
 
-enum {DTSEC, XGMAC};
+#define MDIO_DATA(x)		(x & 0xffff)
+#define MDIO_DATA_BSY		(1 << 31)
 
-struct mac_device {
-	struct device		*dev;
-	void			*priv;
-	uint8_t			 cell_index;
-	struct resource		*res;
-	void			*vaddr;
-	uint8_t			 addr[ETH_ALEN];
-	bool			 promisc;
-
-	struct fm		*fm_dev;
-	struct fm_port		*port_dev[2];
-
-	phy_interface_t		 phy_if;
-	u32			 if_support;
-	bool			 link;
-	bool			 half_duplex;
-	uint16_t		 speed;
-	uint16_t		 max_speed;
-	struct device_node	*phy_node;
-	struct device_node	*tbi_node;
-	struct phy_device	*phy_dev;
-	void			*fm;
-
-	int (*init_phy)(struct net_device *net_dev);
-	int (*init)(struct mac_device *mac_dev);
-	int (*start)(struct mac_device *mac_dev);
-	int (*stop)(struct mac_device *mac_dev);
-	int (*change_promisc)(struct mac_device *mac_dev);
-	int (*uninit)(struct mac_device *mac_dev);
-};
-
-static inline void * __attribute((nonnull)) macdev_priv(const struct mac_device *mac_dev)
-{
-	return (void *)mac_dev + sizeof(*mac_dev);
-}
-
-extern const char	*mac_driver_description;
-extern const size_t	 mac_sizeof_priv[];
-extern void (*const mac_setup[])(struct mac_device *mac_dev);
-
-#endif	/* __MAC_H */
+#endif /* __XGMAC_MDIO_H */
