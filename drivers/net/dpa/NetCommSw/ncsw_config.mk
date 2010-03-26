2e6b35bfed6f4a166f85a9f46a5a0d58197902d7
Donio Ron <rdonio@freescale.com>
2010-03-26 15:21:41 +0300

    Freescale NetCommSw other files
    drivers/net/dpa/NetCommSw/dflags.h
    drivers/net/dpa/NetCommSw/etc/error.c
    drivers/net/dpa/NetCommSw/etc/list.c
    drivers/net/dpa/NetCommSw/etc/Makefile
    drivers/net/dpa/NetCommSw/etc/memcpy.c
    drivers/net/dpa/NetCommSw/etc/mm.c
    drivers/net/dpa/NetCommSw/etc/mm.h
    drivers/net/dpa/NetCommSw/etc/sprint.c
    drivers/net/dpa/NetCommSw/events_mapping.h
    drivers/net/dpa/NetCommSw/Kconfig
    drivers/net/dpa/NetCommSw/Makefile
    drivers/net/dpa/NetCommSw/ncsw_config.mk
    
    Signed-off-by: Donio Ron <rdonio@freescale.com>
    Signed-off-by: Emil Medve <Emilian.Medve@Freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>
    Signed-off-by: Shlomi Gridish <gridish@freescale.com>
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

---
+++ drivers/net/dpa/NetCommSw/ncsw_config.mk
@@ -0,0 +1,21 @@
+#
+# Makefile config for the Freescale NetcommSW
+#
+NET_DPA     = $(srctree)/drivers/net
+DRV_DPA     = $(srctree)/drivers/net/dpa
+NCSW        = $(srctree)/drivers/net/dpa/NetCommSw
+
+EXTRA_CFLAGS +=-include $(NCSW)/dflags.h
+
+EXTRA_CFLAGS += -I$(DRV_DPA)/
+EXTRA_CFLAGS += -I$(NCSW)/inc
+EXTRA_CFLAGS += -I$(NCSW)/inc/etc
+EXTRA_CFLAGS += -I$(NCSW)/inc/Peripherals
+EXTRA_CFLAGS += -I$(NCSW)/inc/integrations
+EXTRA_CFLAGS += -I$(NCSW)/inc/integrations/P4080
+EXTRA_CFLAGS += -I$(NCSW)/user/env/linux/kernel/2.6/inc
+EXTRA_CFLAGS += -I$(NCSW)/user/env/linux/kernel/2.6/inc/system
+EXTRA_CFLAGS += -I$(NCSW)/user/env/linux/kernel/2.6/inc/wrappers/Peripherals
+EXTRA_CFLAGS += -I$(NCSW)/user/env/linux/kernel/2.6/inc/ioctl
+EXTRA_CFLAGS += -I$(NCSW)/user/env/linux/kernel/2.6/inc/ioctl/Peripherals
+EXTRA_CFLAGS += -I$(NCSW)/user/env/linux/kernel/2.6/inc/ioctl/integrations/P4080
