af72055ba709ed7378ad13e260c6ac35c4f4e624
Andy Fleming <afleming@freescale.com>
2010-03-26 13:41:19 +0300

    Freescale DPA driver
    drivers/net/dpa/dpa.c
    drivers/net/dpa/dpa-common.h
    drivers/net/dpa/dpa-ethtool.c
    drivers/net/dpa/dpa.h
    drivers/net/dpa/fman-conf.h
    drivers/net/dpa/fm.c
    drivers/net/dpa/fm.h
    drivers/net/dpa/fm-wrapper.c
    drivers/net/dpa/fm-wrapper.h
    drivers/net/dpa/mac-api.c
    drivers/net/dpa/mac.c
    drivers/net/dpa/mac.h
    drivers/net/dpa/Makefile
    drivers/net/dpa/port.c
    drivers/net/dpa/port.h
    drivers/net/dpa/port-wrapper.c
    
    Signed-off-by: Andy Fleming <afleming@freescale.com>
    Signed-off-by: Donio Ron <rdonio@freescale.com>
    Signed-off-by: Emil Medve <Emilian.Medve@Freescale.com>
    Signed-off-by: Geoff Thorpe <Geoff.Thorpe@freescale.com>
    Signed-off-by: Kim Phillips <kim.phillips@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>
    Signed-off-by: Shlomi Gridish <gridish@freescale.com>
    Signed-off-by: Timur Tabi <timur@freescale.com>
    Signed-off-by: Yashpal Dutta <yashpal.dutta@freescale.com>
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

---
+++ drivers/net/dpa/dpa.h
@@ -0,0 +1,81 @@
+/* Copyright (c) 2008 - 2009, Freescale Semiconductor, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *	 notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *	 notice, this list of conditions and the following disclaimer in the
+ *	 documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Freescale Semiconductor nor the
+ *	 names of its contributors may be used to endorse or promote products
+ *	 derived from this software without specific prior written permission.
+ *
+ *
+ * ALTERNATIVELY, this software may be distributed under the terms of the
+ * GNU General Public License ("GPL") as published by the Free Software
+ * Foundation, either version 2 of that License or (at your option) any
+ * later version.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Freescale Semiconductor ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Freescale Semiconductor BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __DPA_H
+#define __DPA_H
+
+#include <linux/ethtool.h>	/* struct ethtool_ops */
+#include <linux/netdevice.h>
+#include <linux/list.h>		/* struct list_head */
+#include <linux/workqueue.h>	/* struct work_struct */
+
+#include "linux/fsl_qman.h"	/* struct qman_fq */
+
+#include "dpa-common.h"
+
+#include "mac.h"		/* struct mac_device */
+
+struct fd_list_head {
+	struct list_head	list;
+	size_t			count, max;
+};
+
+#ifdef CONFIG_FSL_FMAN_TEST
+struct pcd_range {
+	uint32_t			 base;
+	uint32_t			 count;
+};
+#endif /* CONFIG_FSL_FMAN_TEST */
+
+struct dpa_priv_s {
+	struct net_device	*net_dev;
+	struct list_head	 dpa_bp_list;
+
+	uint16_t		 channel;
+	struct list_head	 dpa_fq_list[2];
+	struct qman_fq		*egress_fqs[8];
+#ifdef CONFIG_FSL_FMAN_TEST
+	int					 num;
+	struct pcd_range	 ranges[4];
+#endif /* CONFIG_FSL_FMAN_TEST */
+
+	struct mac_device	*mac_dev;
+
+	struct work_struct	 fd_work;
+	struct fd_list_head	*fd_list;
+
+	uint32_t		 msg_enable;	/* net_device message level */
+};
+
+extern const struct ethtool_ops dpa_ethtool_ops;
+
+#endif	/* __DPA_H */
---
+++ drivers/net/dpa/mac.h
@@ -0,0 +1,80 @@
+/* Copyright (C) 2008-2009 Freescale Semiconductor, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *	 notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *	 notice, this list of conditions and the following disclaimer in the
+ *	 documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Freescale Semiconductor nor the
+ *	 names of its contributors may be used to endorse or promote products
+ *	 derived from this software without specific prior written permission.
+ *
+ *
+ * ALTERNATIVELY, this software may be distributed under the terms of the
+ * GNU General Public License ("GPL") as published by the Free Software
+ * Foundation, either version 2 of that License or (at your option) any
+ * later version.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Freescale Semiconductor ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Freescale Semiconductor BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __MAC_H
+#define __MAC_H
+
+#include <linux/device.h>	/* struct device, BUS_ID_SIZE */
+#include <linux/if_ether.h>	/* ETH_ALEN */
+#include <linux/phy.h>		/* phy_interface_t, struct phy_device */
+
+#include "fsl_fman.h"		/* struct port_device */
+
+enum {DTSEC, XGMAC};
+
+struct mac_device {
+	struct device		*dev;
+	void			*priv;
+	uint8_t			 cell_index;
+	struct resource		*res;
+	void			*vaddr;
+	uint8_t			 addr[ETH_ALEN];
+	bool			 promisc;
+
+	struct fm		*fm_dev;
+	struct fm_port	*port_dev[2];
+
+	phy_interface_t		 phy_if;
+	bool			 link;
+	bool			 half_duplex;
+	uint16_t		 speed;
+	uint16_t		 max_speed;
+	char			 phy_id[BUS_ID_SIZE];
+	struct phy_device	*phy_dev;
+    void			*fm;
+
+	int (*init)(struct mac_device *mac_dev);
+	int (*start)(struct mac_device *mac_dev);
+	int (*stop)(struct mac_device *mac_dev);
+	int (*change_promisc)(struct mac_device *mac_dev);
+	int (*uninit)(struct mac_device *mac_dev);
+};
+
+static inline void * __attribute((nonnull)) macdev_priv(const struct mac_device *mac_dev)
+{
+	return (void *)mac_dev + sizeof(*mac_dev);
+}
+
+extern const char	*mac_driver_description;
+extern const size_t	 mac_sizeof_priv[];
+extern void (*const mac_setup[])(struct mac_device *mac_dev);
+
+#endif	/* __MAC_H */
